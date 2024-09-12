d966a74b2a71f1539609c4eb966ab4616e0bbd9b
jackemp <jackpaalomino@gmail.com>
2024-09-12 10:26:47 -0500

    Primer commit

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
+
+Console.WriteLine("Hello, World!");
+string re = "s";
+double suma = 0;
+double cant = 0;
+while (re == "s") {
+    Console.WriteLine("Desea participar (s/n)?");
+    re = Console.ReadLine();
+    if (re== "s") {
+        Console.WriteLine("Ingrese edad");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        suma = suma + edad;
+        cant = cant + 1;
+    }
+}
+double prom = suma/cant;
+Console.WriteLine($"El promedio de {cant} edades es {prom}");
+
